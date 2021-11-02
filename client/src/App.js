0e87e9cdbe6015018a8eb41e9773d1d6f03a966f
Boris Jančić <jancicboris56@gmail.com>
2021-11-02 22:36:35 +0100

    Sorted out imports and added get request for posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import memories from "./images/memories.png";
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
-import useStyles from './styles'
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import memories from './images/memories.png';
 
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
 
     return (
         <Container maxWidth="lg">
