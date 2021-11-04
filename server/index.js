d7bb43d84b796f7ccc84ae894cbdd98c9674dfd4
Boris Jančić <jancicboris56@gmail.com>
2021-11-04 12:07:34 +0100

    Added enviromental variables

--- server/index.js
+++ server/index.js
@@ -3,18 +3,19 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from "./routes/posts.js"
+import dotenv from 'dotenv'
 
 const app = express() // inits express
-
-app.use("/posts", postRoutes) // every posts in routes will start as http://localhost:5000/posts
+dotenv.config()
 
 app.use(bodyParser.json({limit: "30mb", extended: "true"}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: "true"}))
 app.use(cors())
 
+app.use("/posts", postRoutes) // every posts in routes will start as http://localhost:5000/posts
 // Use database from https://www.mongodb.com/atlas/database
 
-const CONNECTION_URL = "Url to mongo DB" // this is hidden for a reason :)
+const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
