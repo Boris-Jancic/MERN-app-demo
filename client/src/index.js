9178ee95ff177cdcd8617ee7ae02c7b7d3f11fab
Boris Jančić <jancicboris56@gmail.com>
2021-11-04 12:08:19 +0100

    Implemented redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
-import reducers from './reducers/index'
-
+import { reducers } from './reducers';
 
 import App from './App';
+import './index.css'
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App/>
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 );
