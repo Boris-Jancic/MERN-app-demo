a01f31404fbf83b371d508b79284371d73e0db52
Boris Jančić <jancicboris56@gmail.com>
2021-11-04 12:06:58 +0100

    Added update, delete and like functionalities

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import Post from './Post/Post'
-import useStyles from './styles'
 
-const Posts = () => {
-    const classes = useStyles()
-    return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>
+import React, { useEffect } from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+  console.log(posts)
+
+  return (
+    posts.length < 1 ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
     )
-}
+  );
+};
 
-export default Posts
+export default Posts;
