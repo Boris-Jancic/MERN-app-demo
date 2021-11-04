c8792067b8567fc7378a7b0c32246d27da28e306
Boris Jančić <jancicboris56@gmail.com>
2021-11-04 12:07:12 +0100

    Fixed typo

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 const postSchema = mongoose.Schema({
     title: String,
-    title: String,
-    title: String,
+    creator: String,
+    message: String,
     tags: [String],
     selectedFile: String, 
     likeCount: {
